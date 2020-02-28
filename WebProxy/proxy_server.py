719f5a6803d8ff58d7d8c33a03b31fa04d00ea17
DevJ5 <idevelopjs@gmail.com>
2020-02-28 16:00:33 +0100

    Web proxy

--- WebProxy/proxy_server.py
+++ WebProxy/proxy_server.py
@@ -16,10 +16,10 @@ serverSocket.listen(5)
 print("The server is ready to receive")
 
 while True:
-    clientSocket, addr = serverSocket.accept()
+    client, addr = serverSocket.accept()
     try:
         print(f"Received connection from {addr}")
-        message = clientSocket.recv(1024).decode()
+        message = client.recv(1024).decode()
         print(message)
         filename = message.split()[1][1:]
         print(filename)
@@ -30,15 +30,26 @@ while True:
                 outputData = f.readlines()
 
             print(outputData)
-            clientSocket.send(b"HTTP/1.0 200 OK\r\n")
-            clientSocket.send(b"Content-Type:text/html\r\n\r\n")
+            client.send(b"HTTP/1.0 200 OK\r\n")
+            client.send(b"Content-Type:text/html\r\n\r\n")
             for i in range(0, len(outputData)):
-                clientSocket.send(outputData[i].encode("utf-8"))
-
+                client.send(outputData[i].encode("utf-8"))
         else:
             print("File does not exist on server")
+            clientSocket = socket(AF_INET, SOCK_STREAM)
+            clientSocket.connect((gethostbyname(filename), 80))
+            #fileobj = clientSocket.makefile('r+b', 0)
+            #fileobj.write(f"GET http://{filename} HTTP/1.0\n\n".encode("utf-8"))
+            #data = fileobj.read()
+            #client.send(data)
+            clientSocket.send(b"GET / HTTP/1.1\r\n\r\n")
+            serverMessage = clientSocket.recv(10000)
+            while len(serverMessage) > 0:
+                client.send(serverMessage)
+                serverMessage = clientSocket.recv(10000)
+            clientSocket.close()
 
-        clientSocket.close()
+        client.close()
         print("Finished...")
     except:
         print(traceback.format_exc())
